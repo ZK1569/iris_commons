a7f11b6af8df82bfc981abe7086d22207ef32479
Cristian URSU <cristian.ursu.2001@gmail.com>
2025-03-14 10:49:28 -0400

    update config

--- env.go
+++ env.go
@@ -1,11 +1,35 @@
 package common
 
-import "syscall"
+import (
+	"os"
+	"strconv"
 
-func EnvString(key, fallback string) string {
-	if val, ok := syscall.Getenv(key); ok {
-		return val
+	"github.com/joho/godotenv"
+)
+
+func GetString(key, fallback string) string {
+	godotenv.Load()
+
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
 	}
 
-	return fallback
+	return val
+}
+
+func GetInt(key string, fallback int) int {
+	godotenv.Load()
+
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	valAsInt, err := strconv.Atoi(val)
+	if err != nil {
+		return fallback
+	}
+
+	return valAsInt
 }
